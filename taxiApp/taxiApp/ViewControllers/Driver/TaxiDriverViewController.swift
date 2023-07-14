ae9fa6b2f5175ac1a7b6a3c0038d29a90896e205
absolute <110104160+skxnz@users.noreply.github.com>
2023-07-14 14:00:14 +0200

    drivers-screen-problems

--- taxiApp/taxiApp/ViewControllers/Driver/TaxiDriverViewController.swift
+++ taxiApp/taxiApp/ViewControllers/Driver/TaxiDriverViewController.swift
@@ -1,7 +1,27 @@
 import UIKit
 
-class TaxiDriverViewController: UICollectionViewController {
+class TaxiDriverViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchRequests()
+    }
+    
+    func fetchRequests() {
+        
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RequestCell", for: indexPath) as? TaxiDriverCollectionViewCell else {
+            fatalError("unable to dequeue reusable cell")
+        }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.bounds.width, height: 150)
     }
 }
---
+++ taxiApp/taxiApp/ViewControllers/Driver/tabBarViewController.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class tabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
