4cf92817cbba33db1d712ad18ee5487891ac043c
absolute <110104160+skxnz@users.noreply.github.com>
2023-07-14 21:38:41 +0200

    No navbar displayed on viewcontroller push

---
+++ taxiApp/taxiApp/ViewControllers/Driver/TaxiDriverCollectionViewCell.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class TaxiDriverCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var salaryLabel: UILabel!
+    @IBOutlet var tripDistanceLabel: UILabel!
+    @IBOutlet var distanceToCustomerLabel: UILabel!
+    @IBOutlet var driverCommentLabel: UILabel!
+    @IBOutlet var destinationLabel: UILabel!
+    @IBOutlet var fromLabel: UILabel!
+}
