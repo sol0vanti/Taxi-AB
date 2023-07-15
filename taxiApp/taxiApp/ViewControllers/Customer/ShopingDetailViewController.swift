e92f1facd42cee286c9f57465ef7ab3d574ea1d8
absolute <110104160+skxnz@users.noreply.github.com>
2023-07-15 13:53:36 +0200

    ❤️‍🩹 New code :D

--- taxiApp/taxiApp/ViewControllers/Customer/ShopingDetailViewController.swift
+++ taxiApp/taxiApp/ViewControllers/Customer/ShopingDetailViewController.swift
@@ -7,12 +7,11 @@ class ShopingDetailViewController: UIViewController {
     @IBOutlet var requestRideButton: UIButton!
     @IBOutlet var driverCommentTextField: UITextField!
     @IBOutlet var tippingSlider: UISlider!
-    @IBOutlet var tarifChoseButton: UIButton!
     @IBOutlet var totalPriceLabel: UILabel!
     @IBOutlet var distanceLabel: UILabel!
     @IBOutlet var errorLabel: UILabel!
     
-    private var taxiTarif = 52
+    private var taxiTarif = 42
     public var distance: Int = 0
     public var distanceToDriver: Int = 0
     private var totalPrice: Int?
@@ -21,33 +20,11 @@ class ShopingDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.tintColor = UIColor.systemPink
-        //setPopupButton()
+
         calculateDistance()
         calculatePrice()
         setUpTippingSlider()
         calculateAdditionalPrice()
-    }
-    
-    func setPopupButton() {
-        let optionClosure = {(action: UIAction) in
-            print(action.title)
-            if action.title == "economy" {
-                self.taxiTarif = 52
-            } else if action.title == "standart" {
-                self.taxiTarif = 74
-            } else if action.title == "business" {
-                self.taxiTarif = 105
-            }
-        }
-        
-        tarifChoseButton.menu = UIMenu(children: [
-            UIAction(title: "economy", state: .on, handler: optionClosure),
-            UIAction(title: "standart", handler: optionClosure),
-            UIAction(title: "business", handler: optionClosure),
-        ])
-        
-        tarifChoseButton.showsMenuAsPrimaryAction = true
-        tarifChoseButton.changesSelectionAsPrimaryAction = true
     }
     
     func calculateDistance() {
@@ -61,9 +38,9 @@ class ShopingDetailViewController: UIViewController {
     }
     
     func setUpTippingSlider() {
-        tippingSlider.value = Float(totalPrice!) * 0.2
+        tippingSlider.value = Float(totalPrice!)
         tippingSlider.minimumValue = Float(totalPrice!) * 0.2
-        tippingSlider.maximumValue = Float(totalPrice!)
+        tippingSlider.maximumValue = Float(totalPrice!) / 1.5
     }
     
     func calculateAdditionalPrice() {
