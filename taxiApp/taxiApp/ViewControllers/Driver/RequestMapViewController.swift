8f5dd05bd5f8c16b6216bcee613c8b0a2183b15a
absolute <110104160+skxnz@users.noreply.github.com>
2023-08-08 10:54:30 +0200

    Version 0.0.1
    Not working firestore

--- taxiApp/taxiApp/ViewControllers/Driver/RequestMapViewController.swift
+++ taxiApp/taxiApp/ViewControllers/Driver/RequestMapViewController.swift
@@ -10,7 +10,7 @@ import MapKit
 import Firebase
 
 enum SkipState {
-    case customerLocation, destination
+    case customerLocation, destination, finish
 }
 
 class RequestMapViewController: UIViewController, MKMapViewDelegate {
@@ -37,8 +37,24 @@ class RequestMapViewController: UIViewController, MKMapViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mapView.delegate = self
+        mapView.mapType = .mutedStandard
+        
         getCoordinates()
         setUpPins()
+        traceARoutes()
+        scheduledTimerWithTimeInterval()
+    }
+    
+    func scheduledTimerWithTimeInterval(){
+        timer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(updateUserLocation), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateUserLocation(){
+        mapView.removeAnnotation(driverPin)
+        driverPin.coordinate = ViewController.driver.coordinate
+        mapView.addAnnotation(driverPin)
     }
     
     func setUpPins() {
@@ -53,6 +69,7 @@ class RequestMapViewController: UIViewController, MKMapViewDelegate {
         destinationPin.coordinate = destinationCoordinate!
         destinationPin.title = "Destination"
         destinationPin.identifier = "destination"
+        
         mapView.addAnnotations([driverPin, customerPin, destinationPin])
     }
     
@@ -61,26 +78,51 @@ class RequestMapViewController: UIViewController, MKMapViewDelegate {
         destinationCoordinate = CLLocationCoordinate2D(latitude: destinationLatitude!, longitude: destinationLongitude!)
     }
     
+    func traceARoutes(){
+            self.drawARoute(ViewController.driver.coordinate, self.userCoordinate!)
+            self.drawARoute(self.userCoordinate!, self.destinationCoordinate!)
+    }
+    
     @IBAction func skipButtonClicked(_ sender: UIButton) {
         switch state {
         case .customerLocation:
+            ViewController.driver.coordinate.latitude = userCoordinate!.latitude - 0.0001
+            ViewController.driver.coordinate.longitude = userCoordinate!.longitude - 0.0001
+            
+            let seconds = 5.0
+            DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
+                let ac = UIAlertController(title: "Already?", message: "Do you see a client?", preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "No", style: .destructive) {_ in
+                    let db = Firestore.firestore()
+                    db.collection("drive-requests").whereField("userNickname", isEqualTo: "\(self.userNickname!)").getDocuments { (snapshot, error) in
+                        
+                    }
+                })
+                ac.addAction(UIAlertAction(title: "Yes", style: .default){ _ in
+                    self.mapView.removeAnnotation(self.customerPin)
+                })
+                self.present(ac, animated: true)
+            }
             state = .destination
-            ViewController.driver.coordinate = userCoordinate!
-            let ac = UIAlertController(title: "Already?", message: "Do you see a client?", preferredStyle: .alert)
-//            ac.addAction(UIAlertAction(title: "No", style: .destructive) {_ in
-//                let db = Firestore.firestore()
-//                db.collection("drive-requests").whereField("orderid", isEqualTo: "\(userNickname)").delete() { error in
-//                    if let error = error {
-//                        print(String(describing: error))
-//                    } else {
-//                        self.navigationController?.popViewController(animated: true)
-//                    }
-//                    
-//                }
-//            })
-            ac.addAction(UIAlertAction(title: "Yes", style: .default))
         case .destination:
-            ViewController.driver.coordinate = destinationCoordinate!
+            ViewController.driver.coordinate.latitude = destinationCoordinate!.latitude - 0.0001
+            ViewController.driver.coordinate.longitude = destinationCoordinate!.longitude - 0.0001
+            
+            skipButton.tintColor = .systemPink
+            skipButton.titleLabel?.text = "Finish"
+            state = .finish
+        case .finish:
+            let seconds = 5.0
+            DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
+                let ac = UIAlertController(title: "Finished?", message: nil, preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "Yes", style: .default) {_ in
+                    let db = Firestore.firestore()
+                    db.collection("drive-requests").whereField("userNickname", isEqualTo: "\(self.userNickname!)").getDocuments { (snapshot, error) in
+                    }
+                    self.navigationController?.popToRootViewController(animated: true)
+                })
+                self.present(ac, animated: true)
+            }
         }
     }
     
@@ -98,5 +140,46 @@ class RequestMapViewController: UIViewController, MKMapViewDelegate {
         return annotationView
     }
     
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.strokeColor = UIColor(red: 17.0/255.0, green: 147.0/255.0, blue: 255.0/255.0, alpha: 1)
+        renderer.lineWidth = 5.0
+        return renderer
+    }
     
+    func drawARoute(_ source: CLLocationCoordinate2D, _ destination: CLLocationCoordinate2D){
+        let sourcePlacemark = MKPlacemark(coordinate: source, addressDictionary: nil)
+        let destinationPlacemark = MKPlacemark(coordinate: destination, addressDictionary: nil)
+        
+        let sourceMapItem = MKMapItem(placemark: sourcePlacemark)
+        let destinationMapItem = MKMapItem(placemark: destinationPlacemark)
+        
+        let directionRequest = MKDirections.Request()
+        directionRequest.source = sourceMapItem
+        directionRequest.destination = destinationMapItem
+        directionRequest.transportType = .automobile
+        
+        let directions = MKDirections(request: directionRequest)
+        
+        directions.calculate {
+            (response, error) -> Void in
+            
+            guard let response = response else {
+                if error != nil {
+                    let ac = UIAlertController(title: "Routes not available", message: "We couldn't get you to this location,     please try another one", preferredStyle: .alert)
+                    ac.addAction(UIAlertAction(title: "OK", style: .default))
+                    self.present(ac, animated: true)
+                } else {
+                    return
+                }
+                return
+            }
+            
+            let route = response.routes[0]
+            self.mapView.addOverlay((route.polyline), level: MKOverlayLevel.aboveRoads)
+            
+            let rect = route.polyline.boundingMapRect
+            self.mapView.setRegion(MKCoordinateRegion(rect), animated: true) 
+        }
+    }
 }
